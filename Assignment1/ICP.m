b26e87ddcbd297586292cdacd3f9caf7b1228415
Vincent Wen <x.wen@student.tue.nl>
2019-04-22 22:47:29 +0200

    Update correct ICP.m

--- Assignment1/ICP.m
+++ Assignment1/ICP.m
@@ -1,44 +1,51 @@
-function [R, t] = ICP(A1, A2)
+function [R, t] = ICP(A1,A2)
 % ICP  Perform Iterative Closest Point algorithm to find R and t from
 % A1(base) to A2(target).
 % Args:
-% A1 [double]: base points of shape N x d 
-% A2 [double]: target points of shape N x d
+% A1 [double]: base points of shape d x N 
+% A2 [double]: target points of shape d x N
 
 % Make backups for original point clouds.
-A1_org = A1;
-A2_org = A2;
+Source = A1;
+Target = A2;
 % Define parameters.
+n1 = size(A1, 2);
+n2 = size(A2, 2);
+d = size(A1, 1);
 epsilon = 1e-6;
-n = size(A1, 1);
-d = size(A1, 2);
-% Initialize R and t.
+% Initialize R = I, t = 0
 R = eye(d);
-t = 0;
+t = zeros(d, 1);
 rms = 9999;
 last_rms = 0;
 iter = 1;
-while abs(rms - last_rms) > epsilon
+while abs(rms - last_rms) > epsilon    
     % Find smallest euclidean distances from A1 to A2 and corresponding
     % mapping.
-    [dists, phi] = pdist2(A2, A1, 'euclidean','Smallest', 1);
-    A2 = A2(phi, :);
+    [dist, phi] = pdist2(Target', A1', 'euclidean', 'Smallest', 1);
+    last_rms = rms;    
+    rms = sqrt(mean(dist));    
+    A2 = Target(:, phi);
     % Compute centroids of A1 and A2.
-    A1c = mean(A1, 1);
-    A2c = mean(A2, 1);
+    A1_bar =mean(A1, 2);
+    A2_bar =mean(A2, 2);
     % Compute centered A1 and A2.
-    A1_centered = A1 - A1c;
-    A2_centered = A2 - A2c;
+    C1=A1 - A1_bar;
+    C2=A2 - A2_bar;
     % Compute the covariance matrix.
-    S = A1_centered' * A2_centered;
-    % Compute SVD and R, t accordingly.
-    [U, Sigma, V] = svd(S);
-    diags = cat(2, ones(1, size(Sigma, 1) - 1), [det(V * U')]);
-    R = V * diag(diags) * U';    
-    t = R * A1c' - A2c';   
-    % Update RMS.
-    last_rms = rms;
-    rms = sqrt(sum(dists) / n);
+    S = C1 * C2';
+    % Compute SVD and R_tmp, t_tmp accordingly.    
+    [U,~,V] = svd(S);
+    diagonal = cat(2, ones(1, size(U, 2) - 1), [det(V * U')]);    
+    R_tmp = V * diag(diagonal) * U';
+    t_tmp = A2_bar - R * A1_bar;
+    % Update R and t.
+    R = R_tmp * R;
+    t = R_tmp * t + t_tmp;
+    % Apply the latest transformation and update A1.
+    A1 = R * Source + t;
     iter = iter + 1;
 end
+sprintf('Iteration: %d', iter);
 end
+
--- Assignment1/test_correctness.m
+++ Assignment1/test_correctness.m
@@ -3,14 +3,10 @@ close all
 
 load('source.mat')
 load('target.mat')
-source = source';
-target = target';
 [R, t] = ICP(source, target);
-tr = bsxfun(@minus, R * source', t);
+tr = bsxfun(@plus, R * source, t);
 figure
 hold on
-plot3(target(:, 1), target(:, 2), target(:, 3), 'k.')
-plot3(source(:, 1), source(:, 2), source(:, 3), 'b.')
+plot3(target(1, :), target(2, :), target(3, :), 'k.')
 plot3(tr(1, :), tr(2, :), tr(3, :), 'r.')
-% plot3(tr(:, 1), tr(:, 2), tr(:, 3), 'r.')
 hold off
