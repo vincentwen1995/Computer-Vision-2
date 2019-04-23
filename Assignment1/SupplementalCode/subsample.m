08d3584f61f19aa0ba1f535c3d5416d03f6b866c
Robbie <luobutang@gmail.com>
2019-04-24 00:33:56 +0200

    Create results folder

--- Assignment1/SupplementalCode/subsample.m
+++ Assignment1/SupplementalCode/subsample.m
@@ -14,13 +14,6 @@ elseif strcmp(type, 'kmeans')
     X_scaled = scale(X, X_mean, X_std);
 
     [centroids, assignments] = vl_kmeans(X_scaled, ncentroids);
-    
-%      size(centroids)
-%      size(X_std)
-%      centroids = bsxfun(@plus, centroids', X_std);
-%      centroids = bsxfun(@times, centroids, X_mean);
-%     
-%      scatter3(centroids(1,:), centroids(2,:), centroids(3,:), 'b');
     subsampled_matrix = [];
     for idx=1:200
         assignment = X(:, assignments == idx);
--- Assignment1/visualize.m
+++ Assignment1/visualize.m
@@ -1,6 +1,5 @@
 clc;close all;
 addpath('SupplementalCode/');
-addpath('fscatter3/');
 load('merging_scenes_1.mat')
 frames=downsample(merged_frame',100)';
 colors=downsample(merged_color',100)';
