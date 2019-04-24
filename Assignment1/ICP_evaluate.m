f626c38111b793507ed2eb891bbd564a2fd356e2
Robbie <luobutang@gmail.com>
2019-04-24 05:57:01 +0200

    update

--- Assignment1/ICP_evaluate.m
+++ Assignment1/ICP_evaluate.m
@@ -2,26 +2,35 @@ clear;clc;close all
 addpath('SupplementalCode')
 [source, target, normals] = load_frames('00', '30');
 
-%% Evaluate noise tolerance 
-[~,~,stats]=ICP_variants(source,target,'normal');
-rms_pure=stats.rms;
-% Define a simple noise
-noise=normrnd(0,0.01,size(source));
-[~,~,stats]=ICP_variants(source+noise,target,'normal');
-rms_noise=stats.rms;
-% [~,~,stats]=ICP_variants(source,target,"uniform");
-% rms_uniform=stats.rms;
-% [~,~,stats]=ICP_variants(source,target,"uniform");
-% rms_random=stats.rms;
-% [~,~,stats]=ICP_variants(source,target,"normal");
-% rms_uniform=stats.rms;
-close all;
-hold on;
-plot(rms_pure);
-plot(rms_noise);
-title("Tolerance to noise for random points selection")
-xlabel("Iterations");
-ylabel("RMS")
-legend('without noise','with noise')
+%% Evaluate noise tolerance
+if false
+    [~,~,stats]=ICP_variants(source,target,'normal');
+    rms_pure=stats.rms;
+    % Define a simple noise
+    noise=normrnd(0,0.01,size(source));
+    [~,~,stats]=ICP_variants(source+noise,target,'normal');
+    rms_noise=stats.rms;
+    % [~,~,stats]=ICP_variants(source,target,"uniform");
+    % rms_uniform=stats.rms;
+    % [~,~,stats]=ICP_variants(source,target,"uniform");
+    % rms_random=stats.rms;
+    % [~,~,stats]=ICP_variants(source,target,"normal");
+    % rms_uniform=stats.rms;
+    close all;
+    hold on;
+    plot(rms_pure);
+    plot(rms_noise);
+    title("Tolerance to noise for random points selection")
+    xlabel("Iterations");
+    ylabel("RMS")
+    legend('without noise','with noise')
+end
 
+%% Evaluate statbility
+if true
+    [~,~,stats]=ICP_variants(source,target,'normal');
+    rms_=stats.rms;
+    
+    
+end
 
