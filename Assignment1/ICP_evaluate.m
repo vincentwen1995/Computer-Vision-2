420b81fdad61e5d531b5bcf342f89b578f88648c
Robbie <luobutang@gmail.com>
2019-04-24 02:22:34 +0200

    Update evaluate

--- Assignment1/ICP_evaluate.m
+++ Assignment1/ICP_evaluate.m
@@ -3,11 +3,11 @@ addpath('SupplementalCode')
 [source, target, normals] = load_frames('00', '30');
 
 %% Evaluate noise tolerance 
-[~,~,stats]=ICP_variants(source,target,"all");
+[~,~,stats]=ICP_variants(source,target,'normal');
 rms_pure=stats.rms;
 % Define a simple noise
 noise=normrnd(0,0.01,size(source));
-[~,~,stats]=ICP_variants(source+noise,target,"all");
+[~,~,stats]=ICP_variants(source+noise,target,'normal');
 rms_noise=stats.rms;
 % [~,~,stats]=ICP_variants(source,target,"uniform");
 % rms_uniform=stats.rms;
@@ -19,7 +19,7 @@ close all;
 hold on;
 plot(rms_pure);
 plot(rms_noise);
-title("Tolerance to noise for all the points selection")
+title("Tolerance to noise for random points selection")
 xlabel("Iterations");
 ylabel("RMS")
 legend('without noise','with noise')
