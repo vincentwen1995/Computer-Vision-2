8cb548c671a0bfcd8c379e8aa5b5cf6243c2b10c
Robbie <luobutang@gmail.com>
2019-05-06 03:52:21 +0200

    Add chaining.m

--- Assignment2/RANSAC.m
+++ Assignment2/RANSAC.m
@@ -1,25 +1,22 @@
-function [F,Inliers]=RANSAC(points1,points2,Iterations,threshold)
+function [F,Inliers]=RANSAC(points1,points2,iterations,threshold)
 if nargin == 3
    threshold=1e-3;
 end
 if nargin == 2
    threshold=1e-3;
-   Iterations=10000;
+   iterations=1e4;
 end
-Inliers=0;
-for i = 1:Iterations
+max_count=0;
+for i = 1:iterations
     sel = randperm(size(points1,2),8);
     p1 = points1(:,sel);
     p2 = points2(:,sel);
     F_p = normalized_eight_point(p1,p2);
-    numerator = (points2' * F_p * points1).^2;
-    fp1 = F_p * points1;
-    fp2 = F_p' * points2;
-    donominator = sum(fp1(1:2,:).^2) + sum(fp2(1:2,:).^2);
-    distance = diag(numerator ./ donominator);
-    count = sum(distance < threshold);
-    if count> Inliers
-        Inliers = count;
+    distance = sampson_distance(points1,points2,F_p); 
+    count = sum(distance <= threshold);
+    if count> max_count
+        max_count = count;
+        Inliers = distance <= threshold;
         F = F_p;
     end
 end
--- Assignment2/Utils/read_frame.m
+++ Assignment2/Utils/read_frame.m
@@ -1,4 +1,5 @@
 function [frame] = read_frame(index)
+    index = mod(index-1 , 49)+1;
     path = sprintf("Data/House/frame%08d.png",index);
     frame = imread(path);
 end
